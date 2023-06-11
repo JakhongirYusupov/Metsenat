27dacae02d261bdc8595ed9cd44bb6310787c9d5
Jakhongir <jakhongiryusupov58@gmail.com>
2023-06-11 22:27:19 +0500

    header have done

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,29 +1,26 @@
 <script>
-  
-  export default {
-    
-  }
+export default {};
 </script>
 
 <template>
   <header class="header py-5 bg-white">
     <div class="container flex justify-between">
-      <img class="w-44 h-6 object-cover" src="@/assets/images/logo.png" alt="">
+      <img class="w-44 h-6 object-cover" src="@/assets/images/logo.png" alt="" />
       <div class="flex items-center gap-11">
-        <div class="header-profile flex items-center bg-gray-200 py-1 pl-5 rounded-md">
-        <p class="mr-6" >Shohrux</p>
-        <div class="w-8 h-8 bg-green-700 flex items-center justify-center rounded mr-1">
-          <i class="fa-solid fa-user" style="color: #ffffff;"></i>
+        <div class="header-profile flex items-center bg-gray-200 py-1 pl-5 rounded-md cursor-pointer">
+          <p class="mr-6">Shohrux</p>
+          <div class="w-8 h-8 bg-green-700 flex items-center justify-center rounded mr-1">
+            <i class="fa-solid fa-user-large fa-lg" style="color: #ffffff"></i>
+          </div>
         </div>
-      </div>
-      <i class="fa-solid fa-arrow-right-from-bracket" style="color: #B1B1B8;"></i>
+        <i class="fa-solid fa-arrow-right-from-bracket fa-lg cursor-pointer" style="color: #b1b1b8"></i>
       </div>
     </div>
   </header>
 </template>
 
 <style>
-  .header{
-    box-shadow: 0px 25px 40px rgba(0, 0, 0, 0.03);
-  }
+.header {
+  box-shadow: 0px 25px 40px rgba(0, 0, 0, 0.03);
+}
 </style>
